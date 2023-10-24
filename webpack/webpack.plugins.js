4fb8e1ee135adb1a3edd4ce196cf4c590c33bd7a
KhaffouKhaffou Mohamed <mohamed.khaffou-khaffou4@etu.univ-lorraine.fr>
2023-10-24 17:15:02 +0200

    fix error to build the project

--- webpack/webpack.plugins.js
+++ webpack/webpack.plugins.js
@@ -6,14 +6,14 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
-/* const environements = new Dotenv({
-	path: path.join(__dirname, '..', `.env`),
+const environements = new Dotenv({
+	/* path: path.join(__dirname, '..', `.env`), */
 	safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
 	allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
 	systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
 	silent: false, // hide any errors
 	defaults: false, // load '.env.defaults' as the default values if empty.
-}) */
+})
 
 // Clean webpack
 const clean = new CleanWebpackPlugin()

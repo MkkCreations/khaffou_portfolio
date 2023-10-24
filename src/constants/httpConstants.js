961154a485726eab41a70415fe4bb4639990fda0
KhaffouKhaffou Mohamed <mohamed.khaffou-khaffou4@etu.univ-lorraine.fr>
2023-10-24 16:20:05 +0200

    config the railway url var

--- src/constants/httpConstants.js
+++ src/constants/httpConstants.js
@@ -1,13 +1,14 @@
 
-export const SKILLS_URL = `${process.env.API_URL}/skills`
-export const PROJECTS_URL = `${process.env.API_URL}/projects`
-export const CONTACT_URL = `${process.env.API_URL}/message`
-export const AUTH_URL = `${process.env.API_URL}/auth`
+export const API_URL = 'http://localhost:8080/api'
+export const SKILLS_URL = `${API_URL}/skills`
+export const PROJECTS_URL = `${API_URL}/projects`
+export const CONTACT_URL = `${API_URL}/message`
+export const AUTH_URL = `${API_URL}/auth`
 export const LOGIN_URL = `${AUTH_URL}/login`
-export const USER_URL = `${process.env.API_URL}/user`
+export const USER_URL = `${API_URL}/user`
 export const USER_DATA_URL = `${USER_URL}/data`
-export const PROFILE_URL = `${process.env.API_URL}/user/me`
+export const PROFILE_URL = `${API_URL}/user/me`
 export const REGISTER_URL = `${AUTH_URL}/register`
 export const LOGOUT_URL = `${AUTH_URL}/logout`
 export const REFRESH_URL = `${AUTH_URL}/refresh-token`
-export const LOGS_URL = `${process.env.API_URL}/logs`
+export const LOGS_URL = `${API_URL}/logs`

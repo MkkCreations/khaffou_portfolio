598527d852dc34c36d476c609c83053243f09467
KhaffouKhaffou Mohamed <mohamed.khaffou-khaffou4@etu.univ-lorraine.fr>
2023-10-24 11:32:38 +0200

    added env variables

--- webpack/webpack.plugins.js
+++ webpack/webpack.plugins.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 const environements = new Dotenv({
-	path: path.join(__dirname, '..', 'environments', `.env`),
+	path: path.join(__dirname, '..', `.env`),
 	safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
 	allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
 	systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
